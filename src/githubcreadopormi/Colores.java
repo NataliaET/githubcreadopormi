1c623e7f05774ba91412d4839e384f2823733209
natrix1 <141676108+natrix1@users.noreply.github.com>
2023-08-09 17:08:37 -0300

    agregue un método
    método pintar si hay colores

--- src/githubcreadopormi/Colores.java
+++ src/githubcreadopormi/Colores.java
@@ -12,4 +12,24 @@ package githubcreadopormi;
 public class Colores {
     
     private int cantColores;
+
+    public Colores(int cantColores) {
+        this.cantColores = cantColores;
+    }
+
+    public int getCantColores() {
+        return cantColores;
+    }
+
+    public void setCantColores(int cantColores) {
+        this.cantColores = cantColores;
+    }
+    public void pintar() {
+    if (cantColores!=0){
+                System.out.println("hay colores para pintar");
+    } else
+        if (cantColores==0){
+            System.out.println("no hay colores para pintar");
+        }
+}
 }
--- src/githubcreadopormi/Githubcreadopormi.java
+++ src/githubcreadopormi/Githubcreadopormi.java
@@ -15,7 +15,8 @@ public class Githubcreadopormi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Colores color = new Colores (3);
+        color.pintar();
     }
     
 }
