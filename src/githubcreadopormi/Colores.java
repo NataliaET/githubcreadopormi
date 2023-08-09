fd911bd19b7aff7f088261e088312ddfaa6eb89b
NataliaET <tato_net@hotmail.com>
2023-08-09 16:47:08 -0300

    Initial commit

---
+++ src/githubcreadopormi/Colores.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubcreadopormi;
+
+/**
+ *
+ * @author Nata
+ */
+public class Colores {
+    
+    private int cantColores;
+}
